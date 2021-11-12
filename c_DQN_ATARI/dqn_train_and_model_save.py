5a7d7e2e31a5789a6a4351f1bf83e343b7c3464f
Youn-Hee Han <yh21.han@gmail.com>
2021-11-13 01:39:10 +0900

    ...

--- c_DQN_ATARI/dqn_train_and_model_save.py
+++ c_DQN_ATARI/dqn_train_and_model_save.py
@@ -308,7 +308,7 @@ def main():
         env_name=ENV_NAME,
         env=env,
         test_env=test_env,
-        use_wandb=False,                        # WANDB 연결 및 로깅 유무
+        use_wandb=True,                        # WANDB 연결 및 로깅 유무
         wandb_entity="link-koreatech",          # WANDB 개인 계정
         max_num_episodes=1_000,                 # 훈련을 위한 최대 에피소드 횟수
         batch_size=32,                          # 훈련시 배치에서 한번에 가져오는 랜덤 배치 사이즈
